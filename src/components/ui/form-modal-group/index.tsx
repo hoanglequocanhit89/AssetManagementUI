93429fdb611d0a6435a88c5af8ab1e4aa3cbe0e8
Vu Tran <vutt1808@gmail.com>
2025-05-20 11:34:30 +0700

    update style detail popup

--- src/components/ui/form-modal-group/index.tsx
+++ src/components/ui/form-modal-group/index.tsx
@@ -9,11 +9,11 @@ const FormModalGroup = ({ title, value }: FormModalProps) => {
 
 
     return (
-        <div className="form-modal__group">
-            <span className="form-modal__group--text">{title}</span>
+        <div className="flex gap-[20px] py-[4px]">
+            <span className="w-[30%] text-[#8e8f91]">{title}</span>
             {
                 typeof(value) === 'string' ? 
-                <span className="form-modal__group--text">{value}</span> : 
+                <span className="grow text-[#8e8f91]">{value}</span> : 
                 <div className="">{value}</div>
             }
         </div>
