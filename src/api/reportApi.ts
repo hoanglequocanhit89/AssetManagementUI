8cce114e8300832760ffe229a854d6cd619e8a52
Vy <vttvy.20it4@vku.udn.vn>
2025-06-09 16:37:50 +0700

    improve: add multiple choices for export by task id 751

--- src/api/reportApi.ts
+++ src/api/reportApi.ts
@@ -1,10 +1,14 @@
-import { BaseResponseWithoutPagination, Report } from "../types";
+import { BaseParams, BaseResponse, BaseResponseWithoutPagination, Report } from "../types";
 import axiosClients from "./axiosClients";
 
 const reportApi = {
     getReportList(): Promise<BaseResponseWithoutPagination<Report[]>> {
-        const url = "reports"
+        const url = "reports/all"
         return axiosClients.get(url);
+    },
+    getReportListWithPagination(params: BaseParams): Promise<BaseResponse<Report>> {
+        const url = "reports"
+        return axiosClients.get(url, { params })
     }
 }
 export default reportApi;
