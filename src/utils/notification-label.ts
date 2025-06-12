b12bde2e7474535775daeccbcf21117e23bc2d2f
thienpham129 <thienversiopp129.it.vn@gmail.com>
2025-06-12 10:00:46 +0700

    feat: ui api integration notification service by task id 770

--- src/utils/notification-label.ts
+++ src/utils/notification-label.ts
@@ -16,14 +16,6 @@ export const generateNotificationMessage = (
             return `${senderName} accepted your assignment with asset (${assetName})`;
         case "ASSIGNMENT_REJECTED":
             return `${senderName} rejected your assignment with asset (${assetName})`;
-        case "USER_RETURN_REQUEST_CREATED":
-            return `${senderName} requested for returning with asset (${assetName}) for you`;
-        case "ANOTHER_ADMIN_RETURN_REQUEST_CREATED":
-            return `${senderName} requested for returning with asset (${assetName}) for you`;
-        case "ANOTHER_ADMIN_RETURN_REQUEST_COMPLETED":
-            return `${senderName} completed request for returning with asset ${assetName}`;
-        case "ANOTHER_ADMIN_RETURN_REQUEST_REJECTED":
-            return `${senderName} rejected your returning request for asset (${assetName})`;
         default:
             return `${senderName} sent a notification about asset (${assetName})`;
     }
